519a7df2fc4571299c7e1e657d51b5392a8ed2e4
IonutP <ionutpanait@yahoo.com>
2022-11-17 12:39:57 +0200

    change console logs to actual text

--- rock-paper-scissors/scripts.js
+++ rock-paper-scissors/scripts.js
@@ -5,22 +5,23 @@ let gameCount = 0;
 let playerSelection;
 let computerSelection;
 
+let summary = document.getElementById('summary');
+
 function getUserChoice() {
     let choice = window.prompt("What's your choice?").toLowerCase();
-    console.log('Player chooses ' + choice);
+    summary.innerHTML += 'Player chooses ' + choice + '. ';
     return choice;
 }
 
 function getComputerChoice(){
     let choices = ['Rock','Paper','Scissors'];
     let randomChoice = choices[Math.floor(Math.random() * 3)].toLowerCase();
-    console.log('Computer has chosen ' + randomChoice);
+    summary.innerHTML += 'Computer has chosen ' + randomChoice + '. ';
     return randomChoice;
 }
 
 function playRound(playerSelection, computerSelection) {
     gameCount++;
-    console.log('Game #' + gameCount + '!');
     return compareChoices(getUserChoice(), getComputerChoice());
 }
 
@@ -28,35 +29,41 @@ function compareChoices(compareChoice, compareToChoice){
     let message = '';
 
     if (compareChoice == compareToChoice) {
-        message = 'It\'s a tie! Score: ' + playerWin + ' - ' + computerWin;
+        message = `It\'s a tie! Score: ${playerWin} - ${computerWin}`;
     } else if (
         (compareChoice == 'rock' && compareToChoice == 'paper') ||
         (compareChoice == 'paper' && compareToChoice == 'scissors') ||
         (compareChoice == 'scissors' && compareToChoice == 'rock')
     ) {
         computerWin++;
-        message = 'Computer won! Score: ' + playerWin + ' - ' + computerWin;
+        message = `Computer won! Score: ${playerWin} - ${computerWin}`;
     } else if (
         (compareChoice == 'rock' && compareToChoice == 'scissors') ||
         (compareChoice == 'paper' && compareToChoice == 'rock') ||
         (compareChoice == 'scissors' && compareToChoice == 'paper')
     ) {
         playerWin++;
-        message = 'You win! Score: ' + playerWin + ' - ' + computerWin;
+        message = `You win! Score: ${playerWin} - ${computerWin}`;
     }
     return message;
 }
 
 function game(){
     for (i = 1; i <= 5; i++) {
-        console.log(playRound(playerSelection, computerSelection));
+        addSummary('Game #' + i + '!');
+        // playRound(playerSelection, computerSelection);
+        addSummary(playRound(playerSelection, computerSelection) + ' <br />');
     }
     if (playerWin == computerWin) {
-        console.log('It\'s a tie! Score was ' + playerWin + ' - ' + computerWin);
+        addSummary(`It's a tie! Score was ${playerWin} - ${computerWin}.`);
     } else if (playerWin > computerWin) {
-        console.log('You are the winner! Score was ' + playerWin + ' - ' + computerWin)
+        addSummary(`You are the winner! Score was ${playerWin} - ${computerWin}.`);
     } else if (computerWin > playerWin) {
-        console.log('Computer is the winner! Score was ' + playerWin + ' - ' + computerWin);
+        addSummary(`Computer is the winner! Score was ${playerWin} - ${computerWin}.`);
     }
+}
+
+function addSummary(message){
+    summary.innerHTML += message + '<br />';
 }
 game();
